494bd46abcf7d2a219243df917b60da8624355b7
LittleSheep2010 <you@example.com>
2020-06-16 20:03:49 +0800

    Lion version, v1.0.2, some change :yum:

--- Lamanda/Lamanda.hpp
+++ Lamanda/Lamanda.hpp
@@ -1,9 +1,17 @@
+/*
+ * @Author: LittleSheep_
+ * @Date: 2020-06-15 22:20:56
+ * @LastEditTime: 2020-06-16 20:02:16
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Documents/Code/GitBuffer/Lamanda-Library/Lamanda/Lamanda.hpp
+ */ 
+
 #ifndef usingLibrary_hpp
 #define usingLibrary_hpp
 
 #include "Console/usingLibrary.hpp"
+#include "Console/outputControl.hpp"
 #include "Data/dataProcessing.hpp"
 #include "Data/temperatureProcessing.hpp"
-using namespace std;
-
 #endif
