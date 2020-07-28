ab621f304574572ef209930f33679d47b6a46cb3
LittleSheep2010 <cadenjiang@outlook.com>
2020-07-28 14:13:34 +0800

    add colorfulPrint

--- Lits/Console/outputControl.hpp
+++ Lits/Console/outputControl.hpp
@@ -15,15 +15,16 @@
 #include <string>
 using namespace std;
 
-string newLine__ = "\n";
-
 class outputControl {
-    private:
-        typedef void noRETURN;
-
     public: /* functions */
+        bool preInit(bool usingWINDOWS_CMDmode);
         bool linePrint(int lineLength, int templateIndex);
-        noRETURN clearScreen(bool usingWINDOWS_CMDmode);
+        bool clearScreen();
+
+    private: /* functions using value */
+        bool isInit;
+
+        bool isUsingWindowsMode;
 
     public: /* values */
         /*
@@ -32,25 +33,44 @@ class outputControl {
          * P.s. all value Index in linePrint function Index is be add 1.
          */
         vector<char> lineTempate;
+        char newLine;
+
+    public: /* subclass */
+        class colorfulPrint {
+            public: /* functions */
+                bool preInit();
+        };
 };
 
-bool outputControl::linePrint(int lineLength, int templateIndex) {
-    string comboString;
-    outputControl::lineTempate.push_back('-');
-    outputControl::lineTempate.push_back('=');
-    outputControl::lineTempate.push_back('*');
-    outputControl::lineTempate.push_back('_');
+bool outputControl::preInit(bool usingWINDOWS_CMDmode) {
+    if(usingWINDOWS_CMDmode) outputControl::isUsingWindowsMode = true;
+    if(!usingWINDOWS_CMDmode) outputControl::isUsingWindowsMode = false;
 
-    // Start combo!
-    for(int i = 1; i <= lineLength; i++) {
-        comboString.append(to_string(lineTempate[templateIndex + 1]));
-    } printf("%s\n", comboString.c_str());
+    outputControl::isInit = true;
+    outputControl::newLine = '\n';
 }
 
-outputControl::noRETURN outputControl::clearScreen(bool usingWINDOWS_CMDmode) {
-    if(!usingWINDOWS_CMDmode) system("clear");
-    else if(usingWINDOWS_CMDmode) system("cls");
-    else return;
+bool outputControl::linePrint(int lineLength, int templateIndex) {
+    if(isInit) {
+        string comboString;
+        outputControl::lineTempate.push_back('-');
+        outputControl::lineTempate.push_back('=');
+        outputControl::lineTempate.push_back('*');
+        outputControl::lineTempate.push_back('_');
+
+        // Start combo!
+        for(int i = 1; i <= lineLength; i++) {
+            comboString.append(to_string(lineTempate[templateIndex + 1]));
+        } printf("%s\n", comboString.c_str());
+    } else return false;
+}
+
+bool outputControl::clearScreen() {
+    if(isInit) {
+        if(!outputControl::isUsingWindowsMode) system("clear");
+        else if(outputControl::isUsingWindowsMode) system("cls");
+        else return false;
+    } else return false;  
 }
 
 #endif
